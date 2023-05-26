26069022a45f955d964d70d55f9f9a73e79f5c2c
giuliofilippi <giuliofilippi@me.com>
2023-05-26 15:36:24 +0100

    commit

--- deep_dream.py
+++ deep_dream.py
@@ -1,72 +1,48 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torchvision.models as models
-import torchvision.transforms as transforms
+from torchvision import models, transforms
 from PIL import Image
 
-def load_image(image_path, transform=None):
-    image = Image.open(image_path)
-    if transform is not None:
-        to_tensor_transform = transforms.ToTensor()
-        image = to_tensor_transform(transform(image)).unsqueeze(0)
-    return image
+def preprocess(image):
+    resize_transform = transforms.Resize((224, 224))
+    transform = transforms.ToTensor()
+    return transform(resize_transform(image)).unsqueeze(0)
 
-def save_image(image, output_path):
-    image = image.clamp(0, 1)
-    image = transforms.ToPILImage()(image.squeeze(0))
-    image.save(output_path)
+def deprocess(tensor):
+    transform = transforms.ToPILImage()
+    return transform(tensor.squeeze(0).cpu())
 
-def deepdream(image, model, layers, iterations, lr, octave_scale, num_octaves):
-    image = image.to(device)
-    model = model.to(device)
-    image = image.requires_grad_(True)
+def deepdream(model, image, iterations, lr, layers):
+    image = preprocess(image).to(device)
+    image.requires_grad = True
 
-    octave_images = [image]
-    for _ in range(num_octaves - 1):
-        size = [int(octave_scale * dim) for dim in octave_images[0].shape[-2:]]
-        high_freq = nn.Upsample(size=size, mode='bilinear', align_corners=False)(octave_images[-1])
-        octave_images.append(high_freq)
+    for i in range(iterations):
+        print(i)
+        model.zero_grad()
+        output = model(image)
 
-    detail = torch.zeros_like(octave_images[-1])
-    for octave_image in octave_images[::-1]:
-        for _ in range(iterations):
-            outputs = model(octave_image)
-            loss = torch.zeros(1, device=device)
-            for layer in layers:
-                target_features = outputs[0][layer]
-                loss += target_features.norm()
+        loss = torch.zeros(1, device=device)
+        for layer in layers:
+            target_features = output[0][layer]
+            loss += target_features.norm()
 
-            octave_image.retain_grad()
-            model.zero_grad()
-            loss.backward(retain_graph=True)
-            detail = lr * octave_image.grad.data / octave_image.grad.data.norm() + detail
+        loss.backward()
+        norm_grad = torch.norm(image.grad.data)
+        image.data += lr * image.grad.data / norm_grad
 
-            octave_image = octave_image + detail
-            octave_image = octave_image.clamp(-1, 1)
+        image.grad.data.zero_()
 
-    return octave_image
+    return deprocess(image)
 
-# Load pre-trained VGG model
-model = models.vgg19(pretrained=True).features
-model.eval()
+# Load pre-trained VGG16 model
+model = models.vgg16(pretrained=True)
+layers = [4, 9, 18, 27, 36]
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-# DeepDream parameters
-layers = [18] #[4, 9, 18, 27, 36]
-iterations = 10 #20
-lr = 0.05
-octave_scale = 1.4
-num_octaves = 4 #10
-resize_transform = transforms.Resize((224, 224))
-
 # Load and preprocess the input image
-input_image = load_image('inputs/cat-and-dog.jpg', transform=resize_transform)
-input_image = input_image.to(device)
-
-# Apply DeepDream
-output_image = deepdream(input_image, model, layers, iterations, lr, octave_scale, num_octaves)
+input_image = Image.open("inputs/cat-and-dog.jpg")
+output_image = deepdream(model, input_image, iterations=40, lr=0.1, layers=layers)
 
 # Save the output image
-save_image(output_image, 'outputs/cat-and-dog.jpg')
+output_image.save("outputs/cat-and-dog.jpg")
